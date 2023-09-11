a28f9ed202747a94b00c0a847df87d833e154081
Joel Oliveira <joel.moliveira@hotmail.com>
2023-09-11 23:06:06 +0200

    worked on the CTA. the main organization is done.

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -7,6 +7,7 @@ import Card from "@mui/material/Card";
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import MKSocialButton from "components/MKSocialButton";
+import MKButton from "components/MKButton";
 
 // Material Kit 2 React examples
 import DefaultNavbar from "examples/Navbars/DefaultNavbar";
@@ -29,7 +30,7 @@ import routes from "routes";
 import footerRoutes from "footer.routes";
 
 // Images
-import bgImage from "assets/images/bg-presentation.jpg";
+// import bgImage from "assets/images/bg-presentation.jpg";
 
 // Copy
 import de from "assets/copy/de.json";
@@ -53,7 +54,7 @@ function Presentation() {
         minHeight="90vh"
         width="100%"
         sx={{
-          backgroundImage: `url(${bgImage})`,
+          // backgroundImage: `url(${bgImage})`,
           backgroundSize: "cover",
           backgroundPosition: "top",
           display: "grid",
@@ -62,28 +63,42 @@ function Presentation() {
       >
         <Container>
           <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
-            <MKTypography
-              variant="h1"
-              color="white"
-              mt={-6}
-              mb={1}
-              sx={({ breakpoints, typography: { size } }) => ({
-                [breakpoints.down("md")]: {
-                  fontSize: size["3xl"],
-                },
-              })}
-            >
-              {copyHomepage.homepage.cta.title}{" "}
-            </MKTypography>
-            <MKTypography
-              variant="body1"
-              color="white"
-              textAlign="center"
-              px={{ xs: 6, lg: 12 }}
-              mt={1}
-            >
-              3d-Drucken Online-Service für jedermann. Klicken und drucken
-            </MKTypography>
+            <Grid item xs={12} md={6}>
+              <MKTypography
+                variant="h1"
+                color="black"
+                mt={-6}
+                mb={1}
+                sx={({ breakpoints, typography: { size } }) => ({
+                  [breakpoints.down("md")]: {
+                    fontSize: size["3xl"],
+                  },
+                })}
+              >
+                {copyHomepage.homepage.cta.title}
+              </MKTypography>
+              <MKButton
+                variant="gradient"
+                color="info"
+                size="large"
+                component="a"
+                href="https://www.creative-tim.com/product/material-kit-react"
+                sx={{ mb: 2 }}
+              >
+                Download Now
+              </MKButton>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <MKTypography
+                variant="body1"
+                color="black"
+                textAlign="left"
+                px={{ xs: 6, lg: 12 }}
+                mt={1}
+              >
+                {copyHomepage.homepage.cta.description}
+              </MKTypography>
+            </Grid>
           </Grid>
         </Container>
       </MKBox>
