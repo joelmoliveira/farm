18bbef3dfc8bd47de13de4eab749151db15a70f3
Joel Oliveira <joel.moliveira@hotmail.com>
2023-09-26 22:27:25 +0200

    footer and partner component revised

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -1,8 +1,6 @@
 // @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 // Material Kit 2 React components
 import MKTypography from "components/MKTypography";
@@ -14,33 +12,25 @@ const date = new Date().getFullYear();
 
 export default {
   brand: {
-    name: "Material Kit 2",
+    name: "Cheesy Layers",
     image: logoCT,
     route: "/",
   },
   socials: [
     {
-      icon: <FacebookIcon />,
-      link: "https://www.facebook.com/CreativeTim/",
-    },
-    {
-      icon: <TwitterIcon />,
-      link: "https://twitter.com/creativetim",
-    },
-    {
-      icon: <GitHubIcon />,
-      link: "https://github.com/creativetimofficial",
-    },
-    {
       icon: <YouTubeIcon />,
-      link: "https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w",
+      link: "",
+    },
+    {
+      icon: <InstagramIcon />,
+      link: "",
     },
   ],
   menus: [
     {
       name: "company",
       items: [
-        { name: "about us", href: "https://www.creative-tim.com/presentation" },
+        { name: "about us", href: "" },
         { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
         { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
         { name: "blog", href: "https://www.creative-tim.com/blog" },
@@ -74,18 +64,7 @@ export default {
   ],
   copyright: (
     <MKTypography variant="button" fontWeight="regular">
-      All rights reserved. Copyright &copy; {date} Material Kit by{" "}
-      <MKTypography
-        component="a"
-        href="https://www.creative-tim.com"
-        target="_blank"
-        rel="noreferrer"
-        variant="button"
-        fontWeight="regular"
-      >
-        Creative Tim
-      </MKTypography>
-      .
+      All rights reserved. Copyright &copy; {date}.
     </MKTypography>
   ),
 };
