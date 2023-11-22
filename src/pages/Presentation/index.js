65e1a60c6f86bf9b5d623d64dc0d179eb8e031ca
Joel Oliveira <joel.moliveira@hotmail.com>
2023-11-22 16:28:08 +0100

    tested accordion component from MUI library on main page. works. to be removed and added elsewhere.

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -26,6 +26,13 @@ import Partner from "pages/Presentation/sections/PartnerUs";
 // Presentation page components
 import BuiltByDevelopers from "pages/Presentation/components/BuiltByDevelopers";
 
+//Accordion
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 // Routes
 import routes from "routes";
 import footerRoutes from "footer.routes";
@@ -107,6 +114,21 @@ function Presentation() {
           boxShadow: ({ boxShadows: { xxl } }) => xxl,
         }}
       >
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Accordion 1</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus
+              ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
         <CountersRevised copy={copyHomepage.homepage.counters} />
         <ContentBlocks />
         {/* <DesignBlocks /> */}
