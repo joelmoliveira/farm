77e3a32f256018a50cc20a38951cd107e9fb0f16
Joel Oliveira <joel.moliveira@hotmail.com>
2023-11-23 15:02:31 +0100

    WRKG. added complete site tree. to be filled

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ import CssBaseline from "@mui/material/CssBaseline";
 // Material Kit 2 React themes
 import theme from "assets/theme";
 import Homepage from "pages/LandingPages/Homepage";
-import HowItWorks from "pages/LandingPages/HowItWorks";
-import AboutUs from "pages/LandingPages/AboutUs";
-import ContactUs from "pages/LandingPages/ContactUs";
+import Tech from "pages/LandingPages/Tech/tech";
+import UseCases from "pages/LandingPages/UseCases/useCases";
+import HowItWorks from "pages/LandingPages/HowItWorks/howWorks";
+import PartnerUs from "pages/LandingPages/PartnerWithUs/partnerUs";
+import AboutUs from "pages/LandingPages/AboutUs/aboutUs";
+import ContactUs from "pages/LandingPages/ContactUs/contactUs";
 
 // Material Kit 2 React routes
 import routes from "routes";
@@ -46,7 +49,10 @@ export default function App() {
         {getRoutes(routes)}
         <Route path="*" element={<Navigate to="/homepage" />} />
         <Route path="/homepage" element={<Homepage />} />
+        <Route path="/tech" element={<Tech />} />
+        <Route path="/usecases" element={<UseCases />} />
         <Route path="/howitworks" element={<HowItWorks />} />
+        <Route path="/partnerus" element={<PartnerUs />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/contactus" element={<ContactUs />} />
       </Routes>
